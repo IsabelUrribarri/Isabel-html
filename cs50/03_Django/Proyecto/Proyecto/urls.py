1937f3204cd9fd41d9596830fd3f121ff8aaaf45
IsabelUrribarri <urribarriisabel@gmail.com>
2022-03-19 16:13:36 -0400

    proyecto 0

--- cs50/03_Django/Proyecto/Proyecto/urls.py
+++ cs50/03_Django/Proyecto/Proyecto/urls.py
@@ -19,5 +19,7 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('myapp/', include("myapp.urls")),
-    path('', include("myapp.urls"))
+    path('', include("myapp.urls")),
+    path('newyear/', include("newyear.urls")),
+    path('tasks/', include("tasks.urls"))
 ]
