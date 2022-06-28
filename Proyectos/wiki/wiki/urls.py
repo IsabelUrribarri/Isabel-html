a0830281ea55f3577b86af6a6c4e29365b09b65a
Maryvi <mcamachog@gmail.com>
2022-06-28 14:56:36 -0400

    give action tu encyclopedia/newpage url

--- Proyectos/wiki/wiki/urls.py
+++ Proyectos/wiki/wiki/urls.py
@@ -19,5 +19,6 @@ from django.urls import include, path
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('encyclopedia/', include("encyclopedia.urls")),
+    path('', include("encyclopedia.urls")),    
 
 ]
